2547b9fb121d27d0770b6a71e6c05d418c5809b3
dioptre <dioptre@gmail.com>
2018-08-13 21:00:57 -0700

    working eclipse

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,18 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/ImpedanceMeasurement_2Wire.c \
 ../src/Timer.c \
 ../src/_write.c \
 ../src/main.c \
 ../src/test_common.c 
 
 OBJS += \
+./src/ImpedanceMeasurement_2Wire.o \
 ./src/Timer.o \
 ./src/_write.o \
 ./src/main.o \
 ./src/test_common.o 
 
 C_DEPS += \
+./src/ImpedanceMeasurement_2Wire.d \
 ./src/Timer.d \
 ./src/_write.d \
 ./src/main.d \
