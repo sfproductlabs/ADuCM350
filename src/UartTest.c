e6390541a8fd25490aacf902587965a3602a30d2
dioptre <dioptre@gmail.com>
2018-09-19 17:52:48 -0700

    working uart

--- src/UartTest.c
+++ src/UartTest.c
@@ -318,12 +318,6 @@ int UartTest(void)
             test_Fail("adi_UART_BufRx() failed");
         }
 
-        char pBuffer[2] = "HIHIHI";
-        int16_t size = 6;
-            /* Print to UART */
-
-            adi_UART_BufTx(hUartDevice, pBuffer, &size);
-
 
         /* copy receive character to transmit */
         if (RxBuffer[0] != 'E')
