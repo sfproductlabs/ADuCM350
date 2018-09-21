955fb9a66c88d5d103e4af8725b288704b9d526b
dioptre <dioptre@gmail.com>
2018-09-21 08:40:11 -0700

    added 'modified from the original'

--- system/include/ADuCM350/adi_int.h
+++ system/include/ADuCM350/adi_int.h
@@ -6,7 +6,8 @@ This software is proprietary to Analog Devices, Inc. and its licensors.  By usin
 this software you agree to the terms of the associated Analog Devices Software 
 License Agreement (See /LICENSES/ADI.LABLAB.LICENSE). 
 
-Portions Copyright (c) 2018 Andrew Grosser, Moritz von Buttlar.
+Modified from the Analog Devices original.
+Portions Copyright (c) 2018 Andrew Grosser, Moritz von Buttlar\.
 
 *********************************************************************************/
 /*!
